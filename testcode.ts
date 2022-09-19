d8dba03123cc956adeed68bb8445ba61791492bf
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-09-19 15:42:52 +0500

    this is all just basics of typescript

---
+++ testcode.ts
@@ -0,0 +1,58 @@
+// import React from 'react';
+// import './App.css';
+// let name: string  = 'farrukh adeel';
+// let age: number | string =21; //union operator
+// let hobbies : string[];
+// let role:[number, string];
+// let school:any; //any data type but not recommended  instead unknown is recommended
+// let unknownType: unknown;
+// // role=[2,'student'];
+// //...........................
+// // function printName(name:string){
+// //   console.log(name);
+// // }
+// // printName('FRK Khan');
+// //...................................
+// let functionName:(name:string)=>void;
+// let funcitonSecondName:(name:number)=>number;
+// let func: (school:string)=> never;
+// //this returns nothing while void returns undefined.
+
+// //......................................
+// //allies : two , type and interface.
+// // type Person = {
+// //    name:string;
+// //    age?:number;//question mark means its optional
+// // }
+// type X = Person & {
+//   name: string;
+//   age:number;
+// }
+// let y:X = {
+//   name:"farrukh",
+//   age:25,
+// }
+// //.....................................
+// //interface
+// interface Person  {
+//   name:string;
+//   age:number;
+// };
+// interface Guy extends Person{
+//   profession:string;
+// }
+// //..............................
+// // let person:Person = {
+// //   name:"farrukhadeelfrk",
+// //   //age: 22,
+// // }
+// // let alotOfPersons:Person[]; //array of person object.
+// function App() {
+//   return (
+//     <div className="App">
+//     Allah Ho Akbar
+//     </div>
+//   );
+// }
+
+// export default App;
