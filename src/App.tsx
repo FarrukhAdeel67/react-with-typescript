7d3fade750b228d456911aa70250618603148549
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-09-20 15:42:23 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import { Todo } from './model';
 
 //React.FC functional component other than that ReactNode supports all the functions
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("")
-  console.log(todo);
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const handleAdd =(e:React.FormEvent)=>{
+    e.preventDefault();
+    if(todo){
+      setTodos([...todos, {id:Date.now(),todo:todo, isDone:false}]);
+      setTodo("");
+    }
+  };
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   );
 }
