4e75ec83e26de62209c3c7193092e27216092c69
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-09-24 23:47:23 +0500

    Update TodoList.tsx

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,6 +3,7 @@ import './style.css'
 import { Todo } from "../model"
 import SingleTodo from './SingleTodo';
 interface Props {
+  
   todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
 }
